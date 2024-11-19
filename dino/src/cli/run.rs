a8cac28b8631cdaf599c0eb567a82651a9216a3d
maya <770257551@qq.com>
2024-11-19 09:27:07 +0800

    feauture: dino macro intojs/fromjs

--- dino/src/cli/run.rs
+++ dino/src/cli/run.rs
@@ -1,20 +1,24 @@
-use anyhow::Result;
-use std::{env, fs};
-
+use crate::{build_project, CmdExecutor, JsWorker, Req};
 use clap::Parser;
-
-use crate::{build_project, CmdExecutor, JsWorker};
+use std::{collections::HashMap, fs};
 
 #[derive(Debug, Parser)]
 pub struct RunOpts {}
 
 impl CmdExecutor for RunOpts {
-    async fn execute(self) -> Result<()> {
-        let cur_dir = env::current_dir()?.display().to_string();
-        let filename = build_project(&cur_dir)?;
-        let content = fs::read_to_string(&filename)?;
+    async fn execute(self) -> anyhow::Result<()> {
+        let filename = build_project(".")?;
+        let content = fs::read_to_string(filename)?;
         let worker = JsWorker::try_new(&content)?;
-        worker.run("await handlers.hello()")?;
+        // TODO: normally this should run axum and let it load the worker
+        let req = Req::builder()
+            .method("GET")
+            .url("https://example.com")
+            .headers(HashMap::new())
+            .build();
+        let ret = worker.run("hello", req)?;
+        println!("Response: {:?}", ret);
+
         Ok(())
     }
 }
--- dino/src/engine.rs
+++ dino/src/engine.rs
@@ -1,10 +1,31 @@
 use anyhow::Result;
-use rquickjs::{Context, Function, Object, Runtime};
+use dino_macros::{FromJs, IntoJs};
+use rquickjs::{Context, Function, Object, Promise, Runtime};
+use std::collections::HashMap;
+use typed_builder::TypedBuilder;
 
 #[allow(unused)]
 pub struct JsWorker {
     rt: Runtime,
     ctx: Context,
+}
+
+#[derive(Debug, TypedBuilder, IntoJs)]
+pub struct Req {
+    pub headers: HashMap<String, String>,
+    #[builder(setter(into))]
+    pub method: String,
+    #[builder(setter(into))]
+    pub url: String,
+    #[builder(default, setter(strip_option))]
+    pub body: Option<String>,
+}
+
+#[derive(Debug, FromJs)]
+pub struct Res {
+    pub status: u16,
+    pub headers: HashMap<String, String>,
+    pub body: Option<String>,
 }
 
 fn print(msg: String) {
@@ -20,35 +41,84 @@ impl JsWorker {
             let global = ctx.globals();
             let ret: Object = ctx.eval(module)?;
             global.set("handlers", ret)?;
-            let fun = Function::new(ctx.clone(), print)
-                .unwrap()
-                .with_name("print")?;
+            // setup print function
+            let fun = Function::new(ctx.clone(), print)?.with_name("print")?;
             global.set("print", fun)?;
+
             Ok::<_, anyhow::Error>(())
         })?;
 
         Ok(Self { rt, ctx })
     }
 
-    pub fn run(&self, code: &str) -> Result<()> {
+    pub fn run(&self, name: &str, req: Req) -> anyhow::Result<Res> {
         self.ctx.with(|ctx| {
-            ctx.eval_promise(code)?.finish()?;
-            Ok::<_, anyhow::Error>(())
-        })?;
-        Ok(())
+            let global = ctx.globals();
+            let handlers: Object = global.get("handlers")?;
+            let fun: Function = handlers.get(name)?;
+            let v: Promise = fun.call((req,))?;
+
+            Ok::<_, anyhow::Error>(v.finish()?)
+        })
     }
 }
+
+// impl<'js> IntoJs<'js> for Request {
+//     fn into_js(self, ctx: &Ctx<'js>) -> rquickjs::Result<Value<'js>> {
+//         let obj = Object::new(ctx.clone())?;
+
+//         obj.set("headers", self.headers)?;
+//         obj.set("method", self.method)?;
+//         obj.set("url", self.url)?;
+//         obj.set("body", self.body)?;
+
+//         Ok(obj.into())
+//     }
+// }
+
+// impl<'js> FromJs<'js> for Response {
+//     fn from_js(_ctx: &Ctx<'js>, v: Value<'js>) -> rquickjs::Result<Self> {
+//         let obj = v.into_object().unwrap();
+
+//         let status: u16 = obj.get("status")?;
+//         let headers: HashMap<String, String> = obj.get("headers")?;
+//         let body: Option<String> = obj.get("body")?;
+
+//         Ok(Response {
+//             status,
+//             headers,
+//             body,
+//         })
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn js_worker_should_work() -> Result<()> {
-        let code = r#"(function(){async function hello(){print("hello world");return"hello";}return{hello:hello};})();"#;
-
+    fn js_worker_should_run() {
+        let code = r#"
+    (function(){
+        async function hello(req){
+            return {
+                status:200,
+                headers:{
+                    "content-type":"application/json"
+                },
+                body: JSON.stringify(req),
+            };
+        }
+        return{hello:hello};
+    })();
+    "#;
+        let req = Req::builder()
+            .method("GET")
+            .url("https://example.com")
+            .headers(HashMap::new())
+            .build();
         let worker = JsWorker::try_new(code).unwrap();
-        worker.run("await handlers.hello()").unwrap();
-        Ok(())
+        let ret = worker.run("hello", req).unwrap();
+        assert_eq!(ret.status, 200);
     }
 }
